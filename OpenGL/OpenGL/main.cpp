aa66b709e6dfe7df861cbe83e3685435677176e8
altair961 <altair89@list.ru>
2024-06-01 22:30:06 +0300

    Wireframe mode was enabled.

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -124,6 +124,8 @@ int main()
 	// 3. then set our vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
